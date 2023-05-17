fbe012a93b067feea3fb524488ed13e61fed8633
Summit <lisummit6@gmail.com>
2023-05-17 11:34:32 -0400

    -Added Product Images -Added Function Add to Cart button -Styled Webpage

---
+++ react_shopping/src/pages/shop/product.jsx
@@ -0,0 +1,27 @@
+import React, { useCallback, useContext } from 'react'
+import {ShopContext} from "../../context/shop-context";
+import { Shop } from './shop';
+
+export const Product = (props) => {
+    const { id, productName, price, productImage } = props.data;
+    const { addToCart, cartItems } = useContext(ShopContext);
+
+    const cartItemAmount = cartItems[id];
+  return (
+    <div className="product">
+        <img src={productImage} />
+        <div className="description">
+            <p>
+                <b>{productName}</b>
+            </p>
+            <p>
+                ${price}
+            </p>
+        </div>
+        <button className="addToCart" onClick={() => addToCart(id)}>
+            Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}
+        </button>
+
+    </div>
+  )
+}
--- react_shopping/src/products.js
+++ react_shopping/src/products.js
@@ -1,11 +1,68 @@
-// Later on use import images 
+import product1 from "./assets/curryjersey.JPG";
+import product2 from "./assets/luffy.JPG";
+import product3 from "./assets/alienmonitor.JPG"
+import product4 from "./assets/corsairmouse.jpg";
+import product5 from "./assets/iphone.JPG";
+import product6 from "./assets/macbook.JPG";
+import product7 from "./assets/razerlaptop.JPG";
+import prodcut8 from "./assets/LOLkeyboard.JPG";
+
 
 export const PRODUCTS = [
     {
         id: 1,
+        productName: "Curry Jersey",
+        price: 1.00,
+        productImage: product1,
+    }, 
+
+    {
+        id: 2,
+        productName: "Luffy Figurine",
+        price: 1.00,
+        productImage: product2,
+    }, 
+
+    {
+        id: 3,
+        productName: "Alien Monitor",
+        price: 1.00,
+        productImage: product3,
+    }, 
+
+    {
+        id: 4,
+        productName: "Corsair Mouse",
+        price: 1.00,
+        productImage: product4,
+    }, 
+
+    {
+        id: 5,
         productName: "iPhone",
         price: 1.00,
-        productImage: "Blank"
-    }
+        productImage: product5,
+    }, 
 
-]
+    {
+        id: 6,
+        productName: "Macbook",
+        price: 1.00,
+        productImage: product6,
+    }, 
+
+    {
+        id: 7,
+        productName: "Razer Laptop",
+        price: 1.00,
+        productImage: product7
+    }, 
+
+    {
+        id: 8,
+        productName: "League of Legends Keyboard",
+        price: 1.00,
+        productImage: prodcut8,
+    }, 
+
+];
