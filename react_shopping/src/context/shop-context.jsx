f6f93fd4b18d04c630c1ec02131da3f7810c7551
Summit <lisummit6@gmail.com>
2023-06-02 22:20:48 -0400

    -Added a build script

--- react_shopping/src/context/shop-context.jsx
+++ react_shopping/src/context/shop-context.jsx
@@ -6,7 +6,7 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let i = 1; i < PRODUCTS.length + 1; i++){
+    for (let i = 1; i < 25; i++){
         cart[i] = 0;
     }
     return cart;
