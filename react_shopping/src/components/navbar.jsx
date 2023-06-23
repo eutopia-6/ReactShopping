0f3ac806ce6afb5e31095660dbd850afd9084db6
Summit <lisummit6@gmail.com>
2023-06-22 23:24:21 -0400

    -Add a sign out

--- react_shopping/src/components/navbar.jsx
+++ react_shopping/src/components/navbar.jsx
@@ -4,23 +4,29 @@ import { ShoppingCart, UserCircle } from 'phosphor-react';
 import { useState, useEffect } from 'react';
 import "./navbar.css";
 import Modal from 'react-modal';
-import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 import jwt_decode from 'jwt-decode';
 
 export const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSignOutOpen, setSignOutOpen] = useState(false);
   const [user, setUser] = useState(0);
   const handleModalOpen = () => {setIsModalOpen(true);};
   const handleModalClose = () => {setIsModalOpen(false)};
-  const login = useGoogleLogin({
-    onSuccess: tokenResponse => console.log(tokenResponse),
-  });
+  const handleSignOutOpen = () => {setSignOutOpen(true)};
+  const handleSignOutClose = () => {setSignOutOpen(false)};
 
   const getUser = (credentialResponse) => {
     var userObject = jwt_decode(credentialResponse);
     setUser(userObject);
     console.log(userObject);
     document.getElementById("user").hidden = true;
+  }
+
+  const userLogOut = () => {
+    setUser(0);
+    handleSignOutClose();
+    document.getElementById("user").hidden = false;
   }
 
   return (
@@ -32,7 +38,12 @@ export const Navbar = () => {
             <Link to="/cart">
                 <ShoppingCart size={40}/>
             </Link>
-            {!(user === 0) && <div className='username'>{user.name}</div>}
+              {!(user === 0) && 
+            <Link onClick={handleSignOutOpen}>
+              <div className='username'>
+                {user.name}
+              </div>
+            </Link>}
             <div id="user">
               <Link onClick={handleModalOpen}>
                 <UserCircle size={40} color='white'/>
@@ -66,6 +77,7 @@ export const Navbar = () => {
               <GoogleLogin
               onSuccess={credentialResponse => {
                 getUser(credentialResponse.credential);
+                handleModalClose();
               }}
               onError={() => {
                 console.log('Login Failed');
@@ -73,6 +85,19 @@ export const Navbar = () => {
               type='icon'
               shape='circle'/>
             </div>
+          </div>
+        </Modal>
+        <Modal 
+        closeTimeoutMS={1000} 
+        isOpen={isSignOutOpen} 
+        onRequestClose={handleSignOutClose} 
+        shouldCloseOnOverlayClick={true}>
+          <div className='signout-modal'>
+            <button 
+                className='signout' 
+                type='submit'
+                onClick={userLogOut}>
+                Sign Out</button>
           </div>
         </Modal>
     </div>
