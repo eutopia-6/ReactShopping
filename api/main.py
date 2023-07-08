b6089ebd20c3474aa867e1c1d7c85d7315dad86c
Summit <lisummit6@gmail.com>
2023-07-07 20:00:10 -0400

    -Configured for flask backend

--- api/main.py
+++ api/main.py
@@ -2,15 +2,15 @@ from flask import Flask, redirect, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://default:PwgKlWk2dtB9@ep-empty-mud-995772.us-east-1.postgres.vercel-storage.com:5432/verceldb'
-#'sqlite:///accounts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://default:PwgKlWk2dtB9@ep-empty-mud-995772-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///accounts.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     email = db.Column(db.String(100))
-
 
 @app.route('/user', methods=['POST', 'GET'])
 def home():
